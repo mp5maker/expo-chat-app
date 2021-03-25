4dac8caee687f56c859e6ae62ca7a7aab5308d26
mp5maker <khan.photon@gmail.com>
2021-03-25 16:45:31 +0600

    feat: simple react native socket io initialization

--- native/App.tsx
+++ native/App.tsx
@@ -1,8 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import socket from "./src/utilties/socket";
 
 export default function App() {
+  React.useEffect(() => {
+    socket.onAny((event, ...args) => {
+      console.log(event, args);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
