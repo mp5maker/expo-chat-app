a741f7e21d376b989e218aa7c03280b23f68775b
mp5maker <khan.photon@gmail.com>
2021-03-28 10:18:17 +0600

    feat: add simple login screen

--- native/src/components/text-input.tsx
+++ native/src/components/text-input.tsx
@@ -1,12 +1,26 @@
-import { TextInput as RNTextInput, TextInputProps } from "react-native";
-import * as React from 'react'
+import {
+  StyleProp,
+  TextInput as RNTextInput,
+  TextInputProps,
+} from "react-native";
+import * as React from "react";
 
-interface ITextInputProps extends TextInputProps {}
+interface ITextInputProps extends TextInputProps {
+  style?: Array<StyleProp<any>>;
+}
 
 const TextInput: React.FC<ITextInputProps> = ({
+  style,
   ...otherProps
 }): JSX.Element => {
   const props = {
+    style: [
+      {
+        minHeight: 40,
+        backgroundColor: "white",
+      },
+      ...(style ? style : []),
+    ],
     ...otherProps,
   };
 
--- native/src/screen/login/index.tsx
+++ native/src/screen/login/index.tsx
@@ -1,13 +1,61 @@
 import * as React from "react";
 import { View } from "../../components/view";
 import { Text } from "../../components/text";
+import TextInput from "../../components/text-input";
+import Button from "../../components/button";
+import { StyleSheet } from "react-native";
 
 const LoginScreen = (): JSX.Element => {
+  const [username, setUsername] = React.useState<string>("");
+
+  const onChangeText = React.useCallback((text) => {
+    setUsername(text);
+  }, []);
+
+  const onPress = React.useCallback(() => {
+    console.log(username);
+  }, [username]);
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={[styles.container]}>
+      <View style={[{ width: "100%", padding: 16, height: "100%" }]}>
+        <View style={[{ marginVertical: 16 }]}>
+          <TextInput
+            style={[
+              {
+                paddingHorizontal: 8,
+              },
+            ]}
+            value={username}
+            onChangeText={onChangeText}
+            placeholder={"Your Username"}
+          />
+        </View>
+        <Button
+          onPress={onPress}
+          style={[
+            {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "lightgrey",
+              minHeight: 40,
+            },
+          ]}
+        >
+          <Text>Send</Text>
+        </Button>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default LoginScreen;
